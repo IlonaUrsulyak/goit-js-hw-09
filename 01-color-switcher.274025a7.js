a8d606755c26b5b2cac077a81d6ac085e9384e31
IlonaUrsulyak <93134592+IlonaUrsulyak@users.noreply.github.com>
2022-06-23 13:14:33 +0000

    Deploying to gh-pages from @ IlonaUrsulyak/goit-js-hw-09@61f818edbc9a42d7de8feb41a568b12ad7fa6daa 🚀

---
+++ 01-color-switcher.274025a7.js
@@ -0,0 +1,2 @@
+!function(){var t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]")},n=null;t.btnStart.disabled=!1,t.btnStart.addEventListener("click",(function(){t.btnStart.disabled=!0,t.btnStop.disabled=!1,n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.btnStop.addEventListener("click",(function(){clearInterval(n),t.btnStop.disabled=!0,t.btnStart.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.274025a7.js.map
